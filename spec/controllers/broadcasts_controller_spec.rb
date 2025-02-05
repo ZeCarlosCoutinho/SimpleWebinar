2f452e4ff5a0fe5414cb91914fb2ae7614390d74
ZeCarlosCoutinho <jczelik@gmail.com>
2025-02-05 16:27:08 +0100

    refactor(broadcasts controller): transform the test file to a controller spec

--- spec/controllers/broadcasts_controller_spec.rb
+++ spec/controllers/broadcasts_controller_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "Broadcasts", type: :request do
+RSpec.describe BroadcastsController, type: :controller do
   let(:broadcast_date1) { Time.zone.now.round(3) } # Round the time to 3 digit milliseconds, which is the default serialization of a datetime
   let(:broadcast_date2) { (broadcast_date1 - 1.day).round(3) }
   let!(:broadcast1) { Broadcast.create(title: "Broadcast 1", broadcast_date: broadcast_date1) }
   let!(:broadcast2) { Broadcast.create(title: "Broadcast 2", broadcast_date: broadcast_date2) }
 
   describe "GET /index" do
+    subject { get :index }
+
     it "returns http success" do
-      get "/broadcasts"
+      subject
       expect(response).to have_http_status(:success)
     end
 
     it "returns the list of broadcasts" do
-      get "/broadcasts"
+      subject
       broadcasts = JSON.parse(response.body)
 
       first_broadcast = broadcasts.first
