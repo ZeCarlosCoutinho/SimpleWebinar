696674e82a9f7db74af8100cf737354e613c6e77
ZeCarlosCoutinho <jczelik@gmail.com>
2025-02-05 15:56:08 +0100

    test(broadcasts controller): fix the check for the date

--- spec/requests/broadcasts_spec.rb
+++ spec/requests/broadcasts_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Broadcasts", type: :request do
-  let!(:broadcast1) { Broadcast.create(title: "Broadcast 1", broadcast_date: Time.zone.now) }
-  let!(:broadcast2) { Broadcast.create(title: "Broadcast 2", broadcast_date: Time.zone.now - 1.day) }
+  let(:broadcast_date1) { Time.zone.now.round(3) } # Round the time to 3 digit milliseconds, which is the default serialization of a datetime
+  let(:broadcast_date2) { (broadcast_date1 - 1.day).round(3) }
+  let!(:broadcast1) { Broadcast.create(title: "Broadcast 1", broadcast_date: broadcast_date1) }
+  let!(:broadcast2) { Broadcast.create(title: "Broadcast 2", broadcast_date: broadcast_date2) }
 
   describe "GET /index" do
     it "returns http success" do
@@ -16,12 +18,12 @@ RSpec.describe "Broadcasts", type: :request do
 
       first_broadcast = broadcasts.first
       expect(first_broadcast['title']).to eq(broadcast1.title)
-      expect(DateTime.parse(first_broadcast['broadcast_date'])).to eq(broadcast1.broadcast_date)
+      expect(Time.parse(first_broadcast['broadcast_date'])).to eq(broadcast1.broadcast_date)
       expect(first_broadcast['subscriptions_count']).to eq(broadcast1.subscriptions_count)
 
       second_broadcast = broadcasts.second
       expect(second_broadcast['title']).to eq(broadcast2.title)
-      expect(DateTime.parse(second_broadcast['broadcast_date'])).to eq(broadcast2.broadcast_date)
+      expect(Time.parse(second_broadcast['broadcast_date'])).to eq(broadcast2.broadcast_date)
       expect(second_broadcast['subscriptions_count']).to eq(broadcast2.subscriptions_count)
     end
   end
